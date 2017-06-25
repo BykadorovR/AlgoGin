cb7649cd519a2d724b4c20ca0aa4ba74b8cb3643
Bykadorov, Roman <prosto_mail95@mail.ru>
2017-06-25 13:39:35 +0300

    batch

--- dependencies/addDependenciesToPath.bat
+++ dependencies/addDependenciesToPath.bat
@@ -1,37 +1,24 @@
 @echo off
-
 set REL_PATH_FREEGLUT=freeglut\
 set REL_PATH_GLEW=glew\
-set ABS_PATH_FREEGLUT=
-set ABS_PATH_GLEW=
 
-rem // Save current directory and change to target directory
-pushd %REL_PATH_FREEGLUT%
+set ABS_PATH_DIR=%~dp0
 
-rem // Save value of CD variable (current directory)
-set ABS_PATH_FREEGLUT=%CD%
+set PATH_LIST_GLUT=%REL_PATH_FREEGLUT% %REL_PATH_FREEGLUT%lib\x64 %REL_PATH_FREEGLUT%lib\Win32 %REL_PATH_FREEGLUT%bin\x64 %REL_PATH_FREEGLUT%bin\Win32
+set PATH_LIST_GLEW=%REL_PATH_GLEW% %REL_PATH_GLEW%lib\Release\x64 %REL_PATH_GLEW%lib\Release\Win32 %REL_PATH_GLEW%bin\Release\x64 %REL_PATH_GLEW%bin\Release\Win32
 
-rem // Restore original directory
-popd
+echo %PATH%
+for %%a in (%PATH_LIST_GLUT%) do (
+   echo %%a
+   echo "%PATH%" | find /C /I "%%a"
+)
+for %%a in (%PATH_LIST_GLEW%) do ( 
+   echo %%a
+)
 
+REM SET path_extended=
+REM reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_SZ /d "%path%;%path_extended%"
 
-rem // Save current directory and change to target directory
-pushd %REL_PATH_GLEW%
+echo %PATH% | find /C /I "%ABS_PATH_FREEGLUT%"
 
-rem // Save value of CD variable (current directory)
-set ABS_PATH_GLEW=%CD%
-
-rem // Restore original directory
-popd
-
-
-echo %ABS_PATH_FREEGLUT%
-echo %ABS_PATH_FREEGLUT%\lib\x64
-echo %ABS_PATH_FREEGLUT%\lib\Win32
-echo %ABS_PATH_FREEGLUT%\bin\x64
-echo %ABS_PATH_FREEGLUT%\bin\Win32;
-echo %ABS_PATH_GLEW%
-echo %ABS_PATH_GLEW%\lib\Release\x64
-echo %ABS_PATH_GLEW%\lib\Release\Win32
-echo %ABS_PATH_GLEW%\bin\Release\x64
-echo %ABS_PATH_GLEW%\bin\Release\Win32
+pause
