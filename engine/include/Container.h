bf34de6293221473acfb81212044f9c04ff9065d
BykadorovR <bykadorov.r@gmail.com>
2021-02-27 22:28:41 +0300

    feat: partly list impl

--- engine/include/Container.h
+++ engine/include/Container.h
@@ -1,19 +1,19 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <optional>
-#include <functional>
 #include <Common.h>
 
 namespace algogin {
-	//Implementation of container - stack, with access by index
+	//Implementation of signle value container with access by index
+	//analog of std::vector
 	template <class T>
 	class List {
 	private:
 		struct Node {
 			T _value;
 			std::shared_ptr<Node> _next;
-			std::shared_ptr<Node> _previous;
 		};
 
 		std::shared_ptr<Node> _head = nullptr;
@@ -21,12 +21,42 @@ namespace algogin {
 	public:
 		List() = default;
 		~List() = default;
+
 		List(const List& list) {
+			_size = list._size;
 
+			std::shared_ptr first = std::make_shared<Node>();
+			first->_value = list[0];
+			_head = first;
+			std::shared_ptr previous = _head;
+			for (int i = 1; i < _size; i++) {
+				std::shared_ptr current = std::make_shared<Node>();
+				current->_value = list[i];
+				previous->_next = current;
+				previous = current;
+			}
 		}
+		
 		void operator=(const List& rhs) {
+			_size = rhs._size;
 
+			std::shared_ptr currentNode = _head;
+			std::shared_ptr previousNode = currentNode;
+			for (int i = 0; i < rhs._size; i++) {
+				if (currentNode == nullptr) {
+					currentNode = std::make_shared<Node>();
+					previousNode->_next = currentNode;
+				}
+
+				currentNode->_value = rhs[i];
+				previousNode = currentNode;
+				currentNode = currentNode->_next;
+			}
+			//break the list if size of current list is bigger than target list
+			//all shared_ptrs in tail should be deallocated
+			previousNode->_next = nullptr;
 		}
+
 		List(const List&& list) noexcept {
 
 		}
@@ -34,32 +64,44 @@ namespace algogin {
 
 		}
 
-		T operator[](int index) {
+		T operator[](int index) const {
 			std::shared_ptr<Node> currentNode = _head;
 			for (int currentIndex = 0; currentIndex < index; currentIndex++) {
 				currentNode = currentNode->_next;
 			}
+
+			if (currentNode == nullptr)
+				throw std::out_of_range("List, operator[]: index is out of range");
+
 			return currentNode->_value;
 		}
 
 		ALGOGIN_ERROR insert(T element, int index) noexcept {
-			if (index > _size)
+			if (index > _size || index < 0)
 				return ALGOGIN_ERROR::OUT_OF_BOUNDS;
 
-			std::shared_ptr<Node> currentNode = _head;
-			for (int currentIndex = 0; currentIndex < index; currentIndex++) {
-				currentNode = currentNode->_next;
-			}
 			std::shared_ptr<Node> newNode = std::make_shared<Node>();
 			newNode->_value = element;
-			newNode->_previous = currentNode;
-			newNode->_next = currentNode->_next;
+
+			std::shared_ptr<Node> currentNode = _head;
+			for (int currentIndex = 0; currentIndex < index - 1; currentIndex++) {
+				currentNode = currentNode->_next;
+			}
+
+			if (index == 0) {
+				newNode->_next = _head;
+				_head = newNode;
+			}
+			else {
+				newNode->_next = currentNode->_next;
+				currentNode->_next = newNode;
+			}
 			_size++;
 
 			return ALGOGIN_ERROR::OK;
 		}
 
-		ALGOGIN_ERROR remove(T value) noexcept {
+		ALGOGIN_ERROR remove(int index) noexcept {
 			return ALGOGIN_ERROR::OK;
 		}
 		
@@ -71,6 +113,12 @@ namespace algogin {
 			return ALGOGIN_ERROR::OK;
 		}
 		ALGOGIN_ERROR print() noexcept {
+			std::shared_ptr<Node> currentNode = _head;
+			for (int i = 0; i < _size; i++) {
+				std::cout << currentNode->_value << " ";
+				currentNode = currentNode->_next;
+			}
+			std::cout << std::endl;
 			return ALGOGIN_ERROR::OK;
 		}
 		ALGOGIN_ERROR random(std::optional<int> size) noexcept {
--- tests/src/Test.cpp
+++ tests/src/Test.cpp
@@ -5,9 +5,62 @@
 
 int main(int argc, char *argv[])
 {
-	algogin::List<int> list;
-	//std::shared_ptr<algogin::List> list = std::make_shared<algogin::List>();
 	testing::InitGoogleTest(&argc, argv);
 //	::testing::GTEST_FLAG(filter) = "Pointer.Callstack";
 	return RUN_ALL_TESTS();
+}
+
+TEST(List, Insert_out_of_bounds) {
+	algogin::List<int> l;
+	ASSERT_EQ(l.insert(10, -1), algogin::ALGOGIN_ERROR::OUT_OF_BOUNDS);
+	ASSERT_EQ(l.insert(10, 1), algogin::ALGOGIN_ERROR::OUT_OF_BOUNDS);
+}
+
+TEST(List, Insert_correct) {
+	algogin::List<int> l;
+	ASSERT_EQ(l.insert(10, 0), algogin::ALGOGIN_ERROR::OK);
+	ASSERT_EQ(l.insert(11, 1), algogin::ALGOGIN_ERROR::OK);
+	ASSERT_EQ(l.insert(12, 1), algogin::ALGOGIN_ERROR::OK);
+	ASSERT_EQ(l.insert(13, 0), algogin::ALGOGIN_ERROR::OK);
+}
+
+TEST(List, Access_operator) {
+	algogin::List<int> l;
+	l.insert(10, 0);
+	l.insert(11, 1);
+	l.insert(12, 1);
+	l.insert(13, 0);
+	ASSERT_EQ(l[0], 13);
+	ASSERT_EQ(l[1], 10);
+	ASSERT_EQ(l[2], 12);
+	ASSERT_EQ(l[3], 11);
+}
+
+TEST(List, Copy_constructor) {
+	algogin::List<int> l1;
+	l1.insert(10, 0);
+	l1.insert(11, 1);
+	l1.insert(12, 1);
+	l1.insert(13, 0);
+	algogin::List<int> l2 = l1;
+	l1.insert(15, 0);
+	ASSERT_EQ(l2[0], 13);
+	ASSERT_EQ(l2[1], 10);
+	ASSERT_EQ(l2[2], 12);
+	ASSERT_EQ(l2[3], 11);
+}
+
+TEST(List, Copy_assignment) {
+	algogin::List<int> l1;
+	l1.insert(10, 0);
+	l1.insert(11, 1);
+	l1.insert(12, 1);
+	l1.insert(13, 0);
+	algogin::List<int> l2;
+	l2.insert(-10, 0);
+	l2.insert(-11, 1);
+	l1 = l2;
+	ASSERT_EQ(l1[0], -10);
+	ASSERT_EQ(l2[1], -11);
+	EXPECT_THROW(l2[2], std::out_of_range);
 }
