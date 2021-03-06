5cfd361862fcb979c4b6b7ef56519bc48e5d45d0
BykadorovR <bykadorov.r@gmail.com>
2021-03-06 23:35:27 +0300

    feat: dictionary recoloring

--- engine/include/Dictionary.h
+++ engine/include/Dictionary.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include "Common.h"
 #include <concepts>
+#include <optional>
 
 namespace algogin {
 	//Dictionary implementation based on red-black tree
@@ -26,12 +27,32 @@ namespace algogin {
 		std::shared_ptr<Tree> _head = nullptr;
 		int _size = 0;
 
-		std::shared_ptr<Tree> findParent(Comparable key) {
+		std::shared_ptr<Tree> _findUncle(std::shared_ptr<Tree> currentNode) {
+			std::shared_ptr<Tree> parent, grandParent;
+			if (currentNode)
+				parent = currentNode->parent;
+			else
+				return nullptr;
+			if (parent)
+				grandParent = parent->parent;
+			else
+				return nullptr;
+
+			std::shared_ptr<Tree> uncle = nullptr;
+			if (grandParent->left == parent)
+				uncle = grandParent->right;
+			else
+				uncle = grandParent->left;
+
+			return uncle;
+		}
+
+		std::shared_ptr<Tree> _findParent(Comparable key) {
 			std::shared_ptr<Tree> currentNode = _head;
 			std::shared_ptr<Tree> parent = nullptr;
 			while (currentNode) {
+				parent = currentNode;
 				if (key < currentNode->key) {
-					parent = currentNode->parent;
 					currentNode = currentNode->left;
 				}
 				else if (key > currentNode->key) {
@@ -40,6 +61,10 @@ namespace algogin {
 			}
 
 			return parent;
+		}
+
+		ALGOGIN_ERROR _recolor(std::shared_ptr<Tree> current) {
+
 		}
 
 	public:
@@ -51,22 +76,34 @@ namespace algogin {
 		Dictionary& operator=(Dictionary&& rhs) noexcept;
 
 
-
 		ALGOGIN_ERROR insert(Comparable key, V value) noexcept {
 			std::shared_ptr node = std::make_shared<Tree>();
 			node->key = key;
 			node->value = value;
 			node->color = Color::RED;
+			//tree is empty, so set node to head
 			if (_head == nullptr) {
 				node->color = Color::BLACK;
 				_head = node;
 				return ALGOGIN_ERROR::OK;
 			}
-
-			auto parent = findParent(key);
+			//find place where should we place current key:value pair
+			auto parent = _findParent(key);
 			if (parent == nullptr)
 				return ALGOGIN_ERROR::WRONG_KEY;
 			
+			//insert to specific place in tree
+			if (value < parent->value) {
+				parent->left = node;
+				node->parent = parent;
+			}
+			
+			//check if parent's node color is black then no problem
+			if (node->parent->color == Color::BLACK)
+				return ALGOGIN_ERROR::OK;
+
+			std::shared_ptr<Tree> uncle;
+			auto uncle = _findUncle(node);
 
 
 		}
