26e66da767527f35948841c23400bfe82199eac0
BykadorovR <bykadorov.r@gmail.com>
2021-06-13 19:45:57 +0300

    feat: added bubble sort

--- engine/include/Sorting.h
+++ engine/include/Sorting.h
@@ -2,24 +2,27 @@
 #include <vector>
 
 class Sorting {
+public:
 	Sorting() = default;
 	~Sorting() = default;
 	
 	template <class T>
 	std::vector<T> bubbleSort(std::vector<T> input) {
-		std::vector<T> output;
-		std::copy(input.begin(), input.end(), output);
+		std::vector<T> output(input);
 
-		bool swapped = false;
+		//early stop to guarantee O(n) if array is sorted
 		for (int i = 1; i < output.size(); i++) {
+			bool swapped = false;
 			for (int j = 0; j < output.size() - i; j++) {
 				if (output[j] > output[j + 1]) {
 					std::swap(output[j], output[j + 1]);
 					swapped = true;
 				}
 			}
-
-
+			if (swapped == false)
+				break;
 		}
+
+		return output;
 	}
 };
---
+++ tests/src/Sorting.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "Sorting.h"
+
+TEST(BubbleSorting, Simple) {
+	Sorting sorting;
+
+	std::vector<int> input = { 10, 3, 5, 1, 2, 30, 31 };
+	auto output = sorting.bubbleSort(input);
+
+	ASSERT_EQ(output[0], 1);
+	ASSERT_EQ(output[1], 2);
+	ASSERT_EQ(output[2], 3);
+	ASSERT_EQ(output[3], 5);
+	ASSERT_EQ(output[4], 10);
+	ASSERT_EQ(output[5], 30);
+	ASSERT_EQ(output[6], 31);
+
+	input = { 5, 1, 4, 2, 8 };
+	output = sorting.bubbleSort(input);
+	ASSERT_EQ(output[0], 1);
+	ASSERT_EQ(output[1], 2);
+	ASSERT_EQ(output[2], 4);
+	ASSERT_EQ(output[3], 5);
+	ASSERT_EQ(output[4], 8);
+}
+
