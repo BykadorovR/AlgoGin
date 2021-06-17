1245ee477cd3fe448f389c5f926b4521b5312159
BykadorovR <bykadorov.r@gmail.com>
2021-06-17 22:39:06 +0300

    feat: added insertion sort

--- engine/include/Sorting.h
+++ engine/include/Sorting.h
@@ -41,4 +41,23 @@ public:
 
 		return output;
 	}
+
+	template <class T>
+	std::vector<T> insertionSort(std::vector<T> input) {
+		std::vector<T> output(input);
+		//we divide array to 2 parts: sorted and unsorted
+		//first element is already sorted so pick starting from second
+		for (int i = 1; i < output.size(); i++) {
+			int j = i;
+			//swap all elements to one element right if current element less than previous one
+			while (j > 0) {
+				if (output[j] < output[j - 1]) {
+					std::swap(output[j], output[j - 1]);
+				}
+				j--;
+			}
+		}
+
+		return output;
+	}
 };
